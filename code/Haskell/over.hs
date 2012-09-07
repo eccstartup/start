35c0b2b70c70e72efbeccef6472e51a002340eea
eccstartup <zhiwudazhanjiangshi@gmail.com>
2012-09-07 21:24:21 +0800

    Fix type info of functions.

--- code/Haskell/over.hs
+++ code/Haskell/over.hs
@@ -1,8 +1,7 @@
 module Main where
-    --above_average :: [Num] -> [Num]
-    --above_average [x] = [x]
+    above_average :: [Int] -> [Int]
     above_average x = filter ( > ave(y) ) x
         where y = x
 
-    --ave :: Num x => [x] -> x
+    ave :: [Int] -> Int
     ave x = (sum x) `div` (length x)
